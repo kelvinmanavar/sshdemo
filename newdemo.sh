729b7bb6c99a19b1dff356aae290b75ad3045303
kelvin <kelvinmanavar86@gmail.com.com>
2022-11-11 16:37:52 +0530

    first commit

--- newdemo.sh
+++ newdemo.sh
@@ -2,16 +2,11 @@
 for i in {0..10}
 do
 	echo hello > $i.txt
-#
-    zip -m multiplefiles.zip $i.txt
-#
-    git config --global user.email "kelvinmanavar86@gmail.com.com"
-    git config --global user.name "kelvin"
-    
-    git init /home/ubantu/Downloads/task01
-    git add *
-    git commit -m "first commit"
-    #git remote add origin git@github.com:kelvinmanavar/sshdemo.git
-    git push -u origin master
-
 done
+
+zip -m multiplefiles.zip *.txt
+git init /home/ubantu/Downloads/task01
+git add *
+git commit -m "first commit"
+#git remote add origin git@github.com:kelvinmanavar/sshdemo.git
+git push -u origin master
